9fa7ff94741bcbccb60c805acbe9be3cca5c420b
Roland Gulya <mgulyaroland@gmail.com>
2024-02-28 14:15:36 +0100

    HitelCalc Project added

---
+++ WebServer/Gyak/HitelCalc/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebServer.Gyak.HitelCalc
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
